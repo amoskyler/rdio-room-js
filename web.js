2310c52bb6f25f43f4decc2fb441f0ea1f5b4994
amoskyler <amos.kyler@gmail.com>
2014-04-13 09:02:51 -0700

    finalizing message

--- web.js
+++ web.js
@@ -46,8 +46,7 @@ app.get("/api/notify-new/", function(req, res){
     console.log(typeof match);
     var body;
     if(match === "true"){
-        body = song +" by "+artist+" has been added to the queue!\n\n#ThanksRdio\n
-            -sent via Twilio"; //song + " by "
+        body = song +" by "+artist+" has been added to the queue!\n\n#ThanksRdio\n-sent via Twilio"; //song + " by "
     }
     else{
         body = "Your song was not found. Sorry. Try again.";
