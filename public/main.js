b099f10fd82d4d44a8c2074c756d8d8c6e91c391
amoskyler <amos.kyler@gmail.com>
2014-04-13 08:25:19 -0700

    Get you're own computer @liddiard

--- public/main.js
+++ public/main.js
@@ -9,7 +9,7 @@ socket.on('sms', function(data) {
 $(document).ready(function(){
     $('#tags').keypress(function(event){
         if (event.which === 13 && $(this).length > 0)
-            addSongToEndOfQueue($(this).text(), '', false);
+            addSongToEndOfQueue($(this).val(), '', false);
     });
 });
 
