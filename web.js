cde56d873121dd3cb90e9f8fe8e698b8005748b8
amoskyler <amos.kyler@gmail.com>
2014-04-13 01:50:36 -0700

    Made some changes to index.html -->client side socket

--- web.js
+++ web.js
@@ -1,15 +1,11 @@
-//web.js
 var express = require('express');
-//var logfmt = require('logfmt');
-//var fs = require('fs');
 var socketsio = require('socket.io');
-//var rdio = require('rdio');
 
 
 
 var app = express();
 var server = require ('http').createServer(app);
-
+app.use(express.static( __dirname  + '/public' ));
 var sockets = [];
 
 var port = Number(process.env.PORT || 5000);
@@ -36,44 +32,3 @@ io.sockets.on('connection', function(socket) {
     sockets.push(socket);
     console.log("socket connected");
 })
-
-
-
-
-
-
-
-
-
-
-/*
-app.use(logfmt.requestLogger());
-app.use(express.static( __dirname  + '/public' ));
-
-app.get('/api/sms', function(req, res) {
-    var body = req.query["Body"]
-    console.log(body);
-    // your business logic here
-    if(typeof(body) !== "undefined"){
-        sockets.forEach(function( socket ){
-        socket.emit('query', body);
-        console.log(socket.emit('query', body));
-    });
-}
-});
-
-app.get('/', function( req, res ){
-    res.sendfile( __dirname + '/index.html' );
-});
-
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-    console.log("Listening on " + port);
-});
-
-var io = require('socket.io').listen( 3030 );
-
-io.sockets.on('connection', function (socket) {
-  sockets.push( socket );
-});
-*/
