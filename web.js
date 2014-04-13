e1c4cfef3e922ec8499acb6c5fc360aa191ffed5
amoskyler <amos.kyler@gmail.com>
2014-04-12 21:31:21 -0700

    Maaaagiiiccc

--- web.js
+++ web.js
@@ -1,27 +1,39 @@
 // web.js
 var express = require('express');
 var logfmt = require('logfmt');
-var socketio = require('socket.io');
+var fs = require('fs');
+var socketsio = require('socket.io');
 var rdio = require('rdio');
 var app = express();
+var sockets = [];
 
 app.use(logfmt.requestLogger());
+app.use(express.static( __dirname  + '/public' ));
 
 app.get('/api/sms', function(req, res) {
     console.log(req);
     var body = req.query["Body"]
-  console.log(body);
-  // your business logic here
-  if(typeof(body) !== "undefined"){
-        res.send('<Response><Message>'+body+'</Message></Response>');
+    console.log(body);
+    // your business logic here
+    if(typeof(body) !== "undefined"){
+        res.sendFile('<Response><Message>'+body+'</Message></Response>');
     }
+    sockets.forEach(function( socket ){
+        socket.emit('query', body);
+    });
 });
 
-app.get('/', function(req, res){
-
-});
+app.get('/', function( req, res ){
+    res.sendfile( __dirname + '/index.html' );
+})
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+var io = require('socket.io').listen( 3030 );
+
+io.sockets.on('connection', function (socket) {
+  sockets.push( socket );
 });
